9b953c81496b172d8f5b3482983b4d92dfa4ad29
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-03-04 17:08:01 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61572491867501"];
+    const permission = ["100071009500533"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
+    api.sendMessage("You don't have enough permission to use this command. Only My Lord Hussain Can Use It.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];

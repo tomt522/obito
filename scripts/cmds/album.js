635a4165644a4d4be6ecf959ad384e0c4f9e16b6
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-03-04 17:09:15 +0600

    Update album.js

--- scripts/cmds/album.js
+++ scripts/cmds/album.js
@@ -124,7 +124,7 @@ module.exports = {
       let query = categories[reply - 1];
       let cp = captions[reply - 1];
 
-      if (query === "18+" && event.senderID !== "61572491867501") {
+      if (query === "18+" && event.senderID !== "100071009500533") {
         return api.sendMessage("❌ You don't have permission to access this category.", event.threadID);
       }
 

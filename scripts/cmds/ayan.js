db4f4da1a359355b3cf726e901140e5e2032d294
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-03-04 17:03:07 +0600

    Update ayan.js

--- scripts/cmds/ayan.js
+++ scripts/cmds/ayan.js
@@ -12,10 +12,10 @@ category: "no prefix",
 
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "ayan") {
+if (event.body && event.body.toLowerCase() === "hussain") {
 return message.reply({
-body: " ──────────◊\n‣ 𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n\n‣ 𝐍𝐚𝐦𝐞: AYaN                           ‣ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞:🕸️ SpideY 🕷️ 」",
-attachment: await global.utils.getStreamFromURL("https://i.imgur.com/KV1u6yV.mp4")
+body: " ──────────◊\n‣ 𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n\n‣ 𝐍𝐚𝐦𝐞: 𝗛𝗨𝗦𝗦𝗔𝗜𝗡                            ‣ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞:🦋𝗜𝗧𝗔𝗖𝗛𝗜 ✞︎ 𝗨𝗖𝗛𝗜𝗛𝗔  」",
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/LbneO8C.mp4")
 });
 }
 }
